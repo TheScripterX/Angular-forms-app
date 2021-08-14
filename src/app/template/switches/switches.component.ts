1b63eeed3291e93b35082f48a12301cc09bd3ce9
TheScripterX <hamdounwassim94@gmail.com>
2021-08-14 23:25:23 +0000

    V 0.1.5 Template: Validations with Checks & Radios & Switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
+  person = {
+    gender: '',
+    newsletter: true,
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  cgu: boolean = false;
 }
