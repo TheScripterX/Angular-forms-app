6d4ca40cb1fdec8b63bc2b22c237c201b687c608
TheScripterX <hamdounwassim94@gmail.com>
2021-08-14 22:00:26 +0000

    V 0.1.4 Template: C-R-D with dynamic forms & arrays

--- src/app/template/dynamic/dynamic.component.ts
+++ src/app/template/dynamic/dynamic.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DynamicComponent implements OnInit {
+export class DynamicComponent {
+  newValue: string = '';
 
-  constructor() { }
+  person: Person = {
+    name: 'Wassim',
+    favorites: [
+      { id: 1, name: 'PES 2021' },
+      { id: 2, name: 'APEX Legends' },
+    ],
+  };
 
-  ngOnInit(): void {
+  save() {
+    console.log(this.person);
   }
 
+  add() {
+    const newGame: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newValue,
+    };
+    this.person.favorites.push({ ...newGame });
+    this.newValue = '';
+  }
+
+  delete(index: number) {
+    this.person.favorites.splice(index, 1);
+  }
 }
