ae6027950102ee286d74e861c6c46387f7da6819
TheScripterX <hamdounwassim94@gmail.com>
2021-08-15 21:38:16 +0000

    V 0.1.7 Reactive: C-R-D with Dynamic FormControls & FormArrays

--- src/app/reactive/dynamic/dynamic.component.ts
+++ src/app/reactive/dynamic/dynamic.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic',
@@ -6,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class DynamicComponent implements OnInit {
-  a: any;
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.fb.array(
+      [
+        ['PES 2021', Validators.required],
+        ['League of Legends', Validators.required],
+        ['OverWatch', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
 
-  constructor() {}
+  get favoritesArray() {
+    return this.myForm.get('favorites') as FormArray;
+  }
+
+  newGame: FormControl = new FormControl('', Validators.required);
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  isValid(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  addGame() {
+    if (this.newGame.invalid) {
+      return;
+    }
+    this.favoritesArray.push(
+      this.fb.control(this.newGame.value, Validators.required)
+    );
+    this.newGame.reset();
+  }
+
+  deleteGame(index: number) {
+    this.favoritesArray.removeAt(index);
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    // this.myForm.reset();
+  }
 }
