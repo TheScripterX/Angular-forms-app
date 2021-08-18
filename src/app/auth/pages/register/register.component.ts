b046d0640d7c93524e754986c8a4584444c178e8
TheScripterX <hamdounwassim94@gmail.com>
2021-08-18 09:57:12 +0000

    V 0.2.0 Validations: Email

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -10,16 +10,23 @@ export class RegisterComponent implements OnInit {
   // Temporary code --Start
 
   fullNamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   // Temporary code --End
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.fullNamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
   });
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.myForm.reset({
+      name: 'Wassim Hamdoun',
+      email: 'test1@gmail.com',
+    });
+  }
 
   isInvalid(field: string) {
     return this.myForm.get(field)?.invalid && this.myForm.get(field)?.touched;
